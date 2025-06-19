4c7ef4ffafbaf3c8eb9ce66b29ed5ecd23e6ab33
linkin <pablogermanjimenez7@gmail.com>
2025-06-19 12:50:16 -0300

    finalizando ejercicio 15

--- js/ejercicio15.js
+++ js/ejercicio15.js
@@ -1,19 +1,27 @@
 // 15- Realiza un script que cuente el número de vocales que tiene un texto.
 
-const cadena = prompt("Ingrese una cadena de texto:").toUpperCase();//Devuelve el string en mayúsculas.
-let caracter;
-let contador = 0;
+const texto = prompt("Ingresá un texto para contar sus vocales:");
 
-for (let i = 0; i < cadena.length; i++) {
-  caracter = cadena.charAt(i); //recupera el caracter i del string.
-  if (
-    caracter == "A" ||
-    caracter == "E" ||
-    caracter == "I" ||
-    caracter == "O" ||
-    caracter == "U"
-  ) {
-    contador++;
+
+let contadorVocales = 0;
+
+
+const textoEnMinusculas = texto.toLowerCase();
+
+
+for (let i = 0; i < textoEnMinusculas.length; i++) {
+ caracter = textoEnMinusculas.charAt(i);
+
+
+  if (caracter === 'a' ||
+      caracter === 'e' ||
+      caracter === 'i' ||
+      caracter === 'o' ||
+      caracter === 'u') {
+
+    contadorVocales++;
   }
 }
-document.writeln(`Número de Vocales: ${contador}`);
+
+
+document.writeln("El texto '" + texto + "' tiene " + contadorVocales + " vocales.");
